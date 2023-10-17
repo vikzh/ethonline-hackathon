8c69be4f94392233aae74701c4af6a5892304ce4
vikzh <vikt.anmail@gmail.com>
2023-10-17 16:28:19 +0200

    ability to draw on the map added

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
         projection: "orthographic",
         center: [-65, 0],
         background: { fill: "#000", stroke: "#999", opacity: 1, width: 1 },
-        container: "celestial-map17",
+        container: "celestial-map",
         datapath: "https://ofrohn.github.io/data/",
         stars: {
             colors: false,
@@ -26,7 +26,7 @@ export default function App() {
         dsos: {
             show: true,
             colors: false,  // Show DSOs in symbol colors, if not use fill-style
-            style: { fill: "#cccccc", stroke: "#cccccc", width: 2, opacity: 1 }, // Default style for dsos
+            style: { fill: "#cccccc", stroke: "#cccccc", width: 10, opacity: 1 }, // Default style for dsos
             names: true,   //Show DSO names
             namestyle: { fill: "#cccccc", font: "12px 'Lucida Sans Unicode', Trebuchet, Helvetica, Arial, sans-serif", align: "left", baseline: "bottom" }
         },
@@ -37,19 +37,105 @@ export default function App() {
 
     useEffect(() => {
         Celestial.display(config);
-        Celestial.skyview({ date: date });
+        handleDateChange()
     }, [date]);
 
     const handleDateChange = () => {
-        setDate(Date.now());
+        var lineStyle = {
+                stroke:"#f00",
+                fill: "rgba(255, 204, 204, 0.4)",
+                width: 3,
+                opacity: 0.6
+            },
+            textStyle = {
+                fill:"#f00",
+                font: "bold 15px Helvetica, Arial, sans-serif",
+                align: "center",
+                baseline: "middle"
+            };
+
+        var jsonLine = {
+            "type":"FeatureCollection",
+            // this is an array, add as many objects as you want
+            "features":[
+                {"type":"Feature",
+                    "id":"SummerTriangle",
+                    "properties": {
+                        // Name
+                        "n":"Summer Triangle",
+                        // Location of name text on the map
+                        "loc": [-67.5, 52]
+                    }, "geometry":{
+                        // the line object as an array of point coordinates
+                        "type":"MultiLineString",
+                        "coordinates":[[
+                            [-80.7653, 38.7837],
+                            [-62.3042, 8.8683],
+                            [-49.642, 45.2803],
+                            [-80.7653, 38.7837]
+                        ]]
+                    }
+                }
+            ]};
+
+        Celestial.add({
+            type:"line",
+
+            callback: function(error, json) {
+
+                if (error) return console.warn(error);
+                // Load the geoJSON file and transform to correct coordinate system, if necessary
+                var asterism = Celestial.getData(jsonLine, config.transform);
+
+                // Add to celestial objects container in d3
+                Celestial.container.selectAll(".asterisms")
+                    .data(asterism.features)
+                    .enter().append("path")
+                    .attr("class", "ast");
+                // Trigger redraw to display changes
+                Celestial.redraw();
+            },
+
+            redraw: function() {
+
+                // Select the added objects by class name as given previously
+                Celestial.container.selectAll(".ast").each(function(d) {
+                    // Set line styles
+                    Celestial.setStyle(lineStyle);
+                    // Project objects on map
+                    Celestial.map(d);
+                    // draw on canvas
+                    Celestial.context.fill();
+                    Celestial.context.stroke();
+
+                    // If point is visible (this doesn't work automatically for points)
+                    if (Celestial.clip(d.properties.loc)) {
+                        // get point coordinates
+                        var pt = Celestial.mapProjection(d.properties.loc);
+                        // Set text styles
+                        Celestial.setTextStyle(textStyle);
+                        // and draw text on canvas
+                        Celestial.context.fillText(d.properties.n, pt[0], pt[1]);
+                    }
+                });
+            }
+        });
+
+        function getPosition(e) {
+            var p = document.getElementById ('celestial-map').getBoundingClientRect(),
+                x = e.offsetX,
+                y = e.offsetY,
+                inv = Celestial.mapProjection.invert([x, y]);
+            console.log(inv);
+            return inv; // [right ascension -180...180 degrees, declination -90...90 degrees]
+        }
+        document.getElementById('celestial-map').addEventListener('mousemove', getPosition, false);
     };
 
     return (
         <div className="App">
             <button onClick={handleDateChange}>Change date</button>
-            <div id="map-container">
-                <div id="map"></div>
-            </div>
+                <div id="celestial-map"></div>
         </div>
     );
 }
