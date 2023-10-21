31f3df621ecc147c821cf62521fa532388d99f52
vikzh <vikt.anmail@gmail.com>
2023-10-21 21:04:24 +0200

    dao contract updated

--- hardhat/contracts/DAO.sol
+++ hardhat/contracts/DAO.sol
@@ -21,7 +21,10 @@ contract DAO {
     struct Proposal {
         uint256 id;
         address proposer;
+        string name;
         string description;
+        string x;
+        string y;
         uint256 yesVotes;
         uint256 noVotes;
         bool executed;
@@ -38,7 +41,7 @@ contract DAO {
     mapping(address => bool) public activeProposals;
 
     // Event for a new proposal
-    event NewProposal(uint256 indexed proposalId, address indexed proposer, string description);
+    event NewProposal(uint256 indexed proposalId, address indexed proposer, string name, string description);
 
     // Event for a proposal execution
     event ProposalExecuted(uint256 indexed proposalId, address indexed proposer, address indexed recipient);
@@ -50,25 +53,30 @@ contract DAO {
             address(this),
             SQLHelpers.toCreateFromSchema(
             "id integer primary key," // Notice the trailing comma
-            "val text",
+            "x text,"
+            "y text,"
+            "description text",
             _TABLE_PREFIX
             )
         );
     }
 
     // Function to create a new proposal
-    function createProposal(string memory _description) external {
+    function createProposal(string memory _name, string memory _description, string memory _x, string memory _y) external {
         Proposal memory newProposal = Proposal({
             id: proposals.length,
             proposer: msg.sender,
+            name: _name,
             description: _description,
+            x: _x,
+            y: _y,
             yesVotes: 0,
             noVotes: 0,
             executed: false
         });
         proposals.push(newProposal);
         activeProposals[msg.sender] = true;
-        emit NewProposal(newProposal.id, msg.sender, _description);
+        emit NewProposal(newProposal.id, msg.sender, _name, _description);
     }
 
     // Function to vote on a proposal
@@ -79,7 +87,7 @@ contract DAO {
         } else {
             proposal.noVotes += 1;
         }
-        }
+    }
 
     // Function to execute a proposal
     function executeProposal(uint256 _proposalId) external {
@@ -91,37 +99,24 @@ contract DAO {
         proposal.executed = true;
         activeProposals[proposal.proposer] = false;
 
-        TablelandDeployments.get().mutate(
-            address(this),
-            _tableId,
-            SQLHelpers.toInsert(
-            _TABLE_PREFIX,
-            _tableId,
-            "id,val",
-            string.concat(
-                Strings.toString(1), // Convert to a string
-                ",",
-                SQLHelpers.quote(Strings.toHexString(msg.sender)) // Wrap strings in single quotes
-            )
-            )
-        );
-        emit ProposalExecuted(_proposalId, proposal.proposer, proposal.proposer);
-    }
-
-    function test(uint id) external {
           TablelandDeployments.get().mutate(
             address(this),
             _tableId,
             SQLHelpers.toInsert(
             _TABLE_PREFIX,
             _tableId,
-            "id,val",
+            "id,x,y,description",
             string.concat(
-                Strings.toString(id), // Convert to a string
+                Strings.toString(proposal.id), // Convert to a string
                 ",",
-                SQLHelpers.quote(Strings.toHexString(msg.sender)) // Wrap strings in single quotes
+                SQLHelpers.quote(proposal.x), // Wrap strings in single quotes
+                ",",
+                SQLHelpers.quote(proposal.y),
+                ",",
+                SQLHelpers.quote(proposal.description)
             )
             )
         );
+        emit ProposalExecuted(_proposalId, proposal.proposer, proposal.proposer);
     }
 }
