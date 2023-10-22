cd8dafc7e9c45fc663f5b1682328642b3c3c73a6
vikzh <vikt.anmail@gmail.com>
2023-10-22 03:13:03 +0200

    hardhat config updated

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,36 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 module.exports = {
-  solidity: "0.8.19",
-};
+  solidity: {
+    version: "0.8.21",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+        details: { yul: false },
+      },
+    },
+  },
+  defaultNetwork: "calibrationnet",
+  networks: {
+    localnet: {
+      chainId: 31415926,
+      url: "http://127.0.0.1:1234/rpc/v1",
+      accounts: [PRIVATE_KEY],
+    },
+    calibrationnet: {
+      chainId: 314159,
+      url: "https://api.calibration.node.glif.io/rpc/v1",
+      accounts: [PRIVATE_KEY],
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+}
+
